e6634fdda650bf4d7d13a7b751d0df02f65a83ee
Travis CI <ci@travis.com>
2019-10-18 10:09:15 -0300

    iniciada a criação da função afnd para a conversão do afnlambda para o afnd e depois para um afd.

--- src/contrloller/Fecho.kt
+++ src/contrloller/Fecho.kt
@@ -7,24 +7,23 @@ import model.Transicoes
 
 class Fecho {
     val tabelaTransicoes = mutableListOf<Transicoes>()
-    fun fechoLambda(afd: Afnl): List<FechoModel> {
-        val fecho = mutableListOf<FechoModel>()
+    val fecho = mutableListOf<FechoModel>()
+    fun fechoLambda(afd: Afnl) {
+
         var aux = FechoModel()
 
         afd.transicoes.forEach {
-            if (it.entrada == "lambda") {
-                if (fecho.isEmpty()) {
+            if (it.entrada == "lambda")
+                if (fecho.isEmpty()){
                     aux.estado = it.inicial
                     aux.entrada = it.entrada
                     aux.fecho = it.final
                     fecho.add(aux)
-                } else {
-                    for (f in fecho) {
-                        if (f.estado == it.inicial) {
-                            if (!f.fecho.contains(it.final)) {
-                                f.fecho = f.fecho + it.final
-                            }
-                        } else {
+                }else{
+                    fecho.forEach { f ->
+                        if (f.estado == it.inicial)
+                            f.fecho = f.fecho + it.final
+                        else{
                             aux.estado = it.inicial
                             aux.entrada = it.entrada
                             aux.fecho = it.final
@@ -32,46 +31,36 @@ class Fecho {
                         }
                     }
                 }
-
-            }
         }
+        println()
         fecho.forEach {
-            for (fim in afd.fim){
-                if (it.fecho.contains(fim))
-                    if (!it.f.contains(it.estado))
-                        it.f.add(it.estado)
-            }
-            /*
             print(it.estado)
             print(" ${it.entrada} ")
             println(it.fecho)
-            println(it.f)
-             */
         }
-        return fecho
     }
 
     fun afnd(afnl: Afnl, fecho: List<FechoModel>){
         val afd: Afd
         var aux = Transicoes()
-        var aux2 = Transicoes()
-
+        var cont = -1
+        val transicoes = mutableListOf<Transicoes>()
         afnl.transicoes.forEach {
-            if (tabelaTransicoes.isEmpty())
-                tabelaTransicoes.add(it)
-            else{
-                //t é o alias do segundo it do foreach
-                tabelaTransicoes.forEach { t ->
-                    if (t.inicial == it.inicial)
-                        if (t.entrada == it.entrada)
-                            if (t.entrada == "lambda")
-                                aux = it
-                            else
-                                t.final = t.final + it.final
-                        else
-                            tabelaTransicoes.add(it)
-                    if (t.entrada == "lambda")
-                        tabelaTransicoes.add(it)
+            transicoes.add(it)
+        }
+
+        aux.inicial = transicoes[0].inicial
+        aux.entrada = transicoes[0].entrada
+        aux.final = transicoes[0].final
+        tabelaTransicoes.add(aux)
+        var controle = false
+        while (controle == false){
+            tabelaTransicoes.forEach {
+                transicoes.forEach { transicao ->
+                    if (it.inicial.contains(transicao.inicial))
+                        if (it.entrada.contains(transicao.entrada))
+                            if (!it.final.contains(transicao.final))
+                                it.final = it.final + transicao.final
                 }
             }
         }
